8c896fef4951746d10b677d01fa8073f5dd32054
doulos76 <inojyes@gmail.com>
2018-04-09 11:53:13 +0900

    Add AlamofireExample

--- Practice/FoodTracker/FoodTracker/MealTableViewController.swift
+++ Practice/FoodTracker/FoodTracker/MealTableViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 dave. All rights reserved.
 //
 
+import os.log
 import UIKit
 
 class MealTableViewController: UITableViewController {
@@ -17,10 +18,16 @@ class MealTableViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    // Use the edit button item provided by the table view controller.
+    navigationItem.leftBarButtonItem = editButtonItem
     
-    // Load the sample data
-    loadSampleMeals()
-    
+    // Load any saved meals, otherwise load sample data.
+    if let savedMeals = loadMeals() {
+      meals += savedMeals
+    } else {
+      // Load the sample data
+      loadSampleMeals()
+    }
   }
   
   override func didReceiveMemoryWarning() {
@@ -60,26 +67,24 @@ class MealTableViewController: UITableViewController {
     return cell
   }
   
+  // Override to support conditional editing of the table view.
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    // Return false if you do not want the specified item to be editable.
+    return true
+  }
   
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
   
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
+  // Override to support editing the table view.
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      // Delete the row from the data source
+      meals.remove(at: indexPath.row)
+      saveMeals()
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+      // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+  }
   
   /*
    // Override to support rearranging the table view.
@@ -96,29 +101,72 @@ class MealTableViewController: UITableViewController {
    }
    */
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    super.prepare(for: segue, sender: sender)
+    switch (segue.identifier ?? "") {
+    case "AddItem":
+      os_log("Addign a new meal", log: OSLog.default, type: .debug)
+      
+    case "ShowDetail":
+      guard let mealDetailViewController = segue.destination as? MealViewController else {
+        fatalError("Unexpected destination: \(segue.destination)")
+      }
+      
+      guard let selectedMealCell = sender as? MealTableViewCell else {
+        fatalError("Unexpected sender: \(sender)")
+      }
+      
+      guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+        fatalError("The selected cell is not beging displayed by the table")
+      }
+      
+      let selectedMeal = meals[indexPath.row]
+      mealDetailViewController.meal = selectedMeal
+      
+    default:
+      fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+    }
+    
+  }
+  
   
   // MARK: Actions
   @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
     if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
-      // Add a new meal
-      let newIndexPath = IndexPath(row: meals.count, section: 0)
       
-      meals.append(meal)
-      tableView.insertRows(at: [newIndexPath], with: .automatic)
+      if let selectedIndexPath = tableView.indexPathForSelectedRow {
+        // Update an existing meal.
+        meals[selectedIndexPath.row] = meal
+        tableView.reloadRows(at: [selectedIndexPath], with: .none)
+      } else {
+        // Add a new meal.
+        let newIndexPath = IndexPath(row: meals.count, section: 0)
+        
+        meals.append(meal)
+        tableView.insertRows(at: [newIndexPath], with: .automatic)
+        
+        // Save the meals
+        saveMeals()
+      }
     }
   }
   
   
   // MARK: Private Methods
+
+  private func saveMeals() {
+    let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+    
+    if isSuccessfulSave {
+      os_log("Meals successfully saved", log: OSLog.default, type: .debug)
+    } else {
+      os_log("Failed to save meals...", log: OSLog.default, type: .error)
+    }
+  }
   
   private func loadSampleMeals() {
     let photo1 = UIImage(named: "meal1")
@@ -141,4 +189,8 @@ class MealTableViewController: UITableViewController {
     
   }
   
+  private func loadMeals() -> [Meal]? {
+    return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+  }
+
 }
---
+++ Practice/MyPlayground-LiftTimeRecording.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var visitCustommar: Bool = false
+
+visitCustommar = true
+
+func cleanOffice() {
+  print("회사 청소의 날!")
+}
+
+if visitCustommar {
+  cleanOffice()
+}
